0b4304745d4457735426154094e1afc0e3791acd
Jesse Pendergrass <compunautics@gmail.com>
2018-08-04 14:54:02 -0500

    More improvements to the set up scripts for minions

--- compunaut_bootstrap.sh
+++ compunaut_bootstrap.sh
@@ -1,28 +1,30 @@
 #!/bin/bash
 set -e
+BLUE='\033[0;34m'
+NC='\033[0m'
 
 # Echo warning to users
   echo -e "#####\nThis script should be run as the root user of your intended ubuntu 16.04 hypervisor server.\n#####"
   sleep 5
 
 # Update sudoers so that sudo group members don't need a password
-  echo -e "\nUpdating sudoers..."
+  echo -e "${BLUE}\nUpdating sudoers...${NC}"
   sed -ri 's/^\%sudo\s+ALL=\(ALL:ALL\)+\sALL$/\%sudo\tALL=\(ALL:ALL\)\ NOPASSWD:ALL/g' /etc/sudoers
 
 # Set up hostname
-  echo -e "\nSetting hostname if not set..."
+  echo -e "${BLUE}\nSetting hostname if not set...${NC}"
   hostnamectl set-hostname salt01
   if [[ ! `grep -P '127.0.1.1\s+salt01' /etc/hosts` ]]; then  
     echo "127.0.1.1 salt01" | tee -a /etc/hosts
   fi
 
 # Update Everything
-  echo -e "\nPerforming updates..."
+  echo -e "${BLUE}\nPerforming updates...${NC}"
   apt-get -qq update
   apt-get -q dist-upgrade -y
 
 # Install Salt Master and Minion
-  echo -e "\nInstalling SaltStack if not installed..."
+  echo -e "${BLUE}\nInstalling SaltStack if not installed...${NC}"
   if [[ ! $(dpkg -l | egrep 'salt-master|salt-minion') ]]; then
     if [[ ! $(apt-key list | grep "SaltStack Packaging Team") ]]; then
       wget -O - https://repo.saltstack.com/apt/ubuntu/16.04/amd64/latest/SALTSTACK-GPG-KEY.pub | sudo apt-key add -
@@ -35,24 +37,24 @@ set -e
   fi
 
 # Autoremove after installations
-  echo -e "\nAutoremoving no-longer-needed software..."
+  echo -e "${BLUE}\nAutoremoving no-longer-needed software...${NC}"
   apt autoremove -y
 
 # Configure Salt Minion to talk to local master
-  echo -e "\nConfiguring local salt minion to talk to salt master..."
+  echo -e "${BLUE}\nConfiguring local salt minion to talk to salt master...${NC}"
   sed -ri 's/^127.0.0.1\s+localhost$/127.0.0.1\tlocalhost\ salt/g' /etc/hosts
   salt-key -A -y
 
 # Clone and Link Compunaut Salt Repos
   # Create base salt directories
-  echo -e "\nCreating salt directories..."
+  echo -e "${BLUE}\nCreating salt directories...${NC}"
   mkdir -pv /srv/{salt,salt-images,pillar,repos}
 
   # Define compunaut formulas in array
   compunaut_repos=( compunaut_default compunaut_hypervisor compunaut_top )
         
   # Clone and link
-  echo -e "\nCloning/fetching Compunaut repos from Github and setting up local directories..."
+  echo -e "${BLUE}\nCloning/fetching Compunaut repos from Github and setting up local directories...${NC}"
   for repo in "${compunaut_repos[@]}"; do
     # Clone repos
     if [[ ! -d /srv/repos/${repo} ]]; then
@@ -82,34 +84,34 @@ set -e
   fi
 
 # Highstate to set up the infrastructure and vms
-  echo -e "\nRefreshing pillars and running highstate..."
+  echo -e "${BLUE}\nRefreshing pillars and running highstate...${NC}"
   salt '*' saltutil.refresh_pillar # refresh pillar before highstate
   sleep 15 # wait a bit
   salt 'salt*' state.highstate # now run highstate
 
 # Wait a bit for the vms to finish booting
   if [[ ! $(virsh list | grep -i compunaut) ]]; then
-    echo -e "\nWait 30 seconds for vms to boot..."
+    echo -e "${BLUE}\nWait 30 seconds for vms to boot...${NC}"
     sleep 30
   fi
 
 # Log into vms and configure salt
-  echo -e "\nLog into vms and configure hostname and salt..."
+  echo -e "${BLUE}\nLog into vms and configure hostname and salt...${NC}"
   for ip in $(virsh net-dumpxml br1 | grep -oP "(?<=ip\=\').+?(?=\'\/>)"); do
     vm=$(virsh net-dumpxml br1 | grep ${ip} | grep -oP "(?<=name\=\').+?(?=\')")
     master_key=$(salt-key -f master.pub | grep -oP '(?<=master.pub:\s\s).+$')
     sshpass -p 'C0mpun4ut1cs!' ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -l compunaut ${ip} \
-      "sudo hostnamectl set-hostname ${vm} && \
-      sudo sed -ri 's/compunaut-minion/${vm}\n172.16.0.1\tsalt/g' /etc/hosts && \
-      sudo sed -ri "s/\#master_finger:\ \'\'/master_finger: ${master_key}/g" /etc/salt/minion && \
+      'sudo hostnamectl set-hostname ${vm} && \
+      sudo sed -ri "s/compunaut-minion/${vm}\n172.16.0.1\tsalt/g" /etc/hosts && \
+      sudo sed -ri "s/#master_finger: \\'\\'/master_finger: ${master_key}/g" /etc/salt/minion && \
       sudo systemctl start salt-minion && \
-      sudo systemctl enable salt-minion"
+      sudo systemctl enable salt-minion'
   done
 
 # Accept all keys
-  echo -e "\nAccept salt keys from vms..."
+  echo -e "${BLUE}\nAccept salt keys from vms...${NC}"
   salt-key -A -y
 
 # Run highstate on all other nodes
-  echo -s "\nRun highstate on all vms..."
+  echo -s "${BLUE}\nRun highstate on all vms...${NC}"
   salt 'compunaut*' state.highstate
