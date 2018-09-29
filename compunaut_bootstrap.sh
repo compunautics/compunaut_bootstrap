c1346fb498a69e40aecc72f968e285919ce20a91
Jesse Pendergrass <compunautics@gmail.com>
2018-09-29 09:05:54 -0500

    Adding a warning to the bootstrap script

--- compunaut_bootstrap.sh
+++ compunaut_bootstrap.sh
@@ -4,14 +4,24 @@ RED='\033[0;31m'
 BLUE='\033[0;34m'
 NC='\033[0m'
 
+### FUNCTIONS
+echo_red() {
+  local message=${1}
+  echo -e "${RED}\n${message}${NC}"
+}
+
+### WARNING TO USERS
+  echo_red "This script should be run as the root user of your intended ubuntu 16.04 hypervisor server."
+  sleep 2
+
 # Set working directory as root dir for this script
-cd "${0%/*}"
+  cd "${0%/*}"
 
 # Set up salt master and minion
-./compunaut_salt_master.sh
+  ./compunaut_salt_master.sh
 
 # Set up repositories
-./compunaut_repo_setup.sh
+  ./compunaut_repo_setup.sh
 
 # Run salt
-./compunaut_run_salt.sh
+  ./compunaut_run_salt.sh
