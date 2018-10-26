a36d18ac2f7d8377f80230be9c102e30afac3648
Jesse Pendergrass <compunautics@gmail.com>
2018-10-26 13:25:23 -0500

    Don't run the full hypervisor state. Might break bridge

--- compunaut_recovery.sh
+++ compunaut_recovery.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # ensure all vms running
-salt -C 'salt* or kvm*' state.apply compunaut_hypervisor,compunaut_consul
+salt -C 'salt* or kvm*' state.apply compunaut_hypervisor.vms,compunaut_consul
 
 # recover databases
 salt '*db*' state.apply compunaut_mysql.galera,compunaut_influxdb
