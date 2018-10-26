931dba88322cd6f14d2e1142c4f7cbe9cda5bb65
Jesse Pendergrass <compunautics@gmail.com>
2018-10-26 10:25:27 -0500

    Adding a recovery script to recover compunaut from a hard outage

---
+++ compunaut_recovery.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# ensure all vms running
+salt -C 'salt* or kvm*' state.apply compunaut_hypervisor,compunaut_consul
+
+# recover databases
+salt '*db*' state.apply compunaut_mysql.galera,compunaut_influxdb
+
+# highstate everything else
+salt 'compunaut*' state.highstate
