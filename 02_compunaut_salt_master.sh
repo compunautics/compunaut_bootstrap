398ad0b430e306fa739db73cca91f9a6cb45fa43
Jesse Pendergrass <compunautics@gmail.com>
2018-11-14 09:09:56 -0600

    Adding numbers to make correct order intuitive

--- 02_compunaut_salt_master.sh
+++ 02_compunaut_salt_master.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -e
 RED='\033[0;31m'
 BLUE='\033[0;34m'
 NC='\033[0m'
@@ -15,20 +14,16 @@ echo_blue() {
   echo -e "${BLUE}\n${message}...${NC}"
 }
 
-### Get positional vars
-minion_hostname=${1}
-salt_master=${2}
-
-### INITIAL SALT SETUP
+### INITIAL SALT MASTER SETUP
 # Update sudoers so that sudo group members don't need a password
   echo_blue "Updating sudoers"
   sed -ri 's/^\%sudo\s+ALL=\(ALL:ALL\)+\sALL$/\%sudo\tALL=\(ALL:ALL\)\ NOPASSWD:ALL/g' /etc/sudoers
 
 # Set up hostname
   echo_blue "Setting hostname if not set"
-  hostnamectl set-hostname ${minion_hostname}
-  if [[ ! `grep -P "127.0.1.1\s+${minion_hostname}" /etc/hosts` ]]; then  
-    echo "127.0.1.1 ${minion_hostname}" | tee -a /etc/hosts
+  hostnamectl set-hostname salt01
+  if [[ ! `grep -P '127.0.1.1\s+salt01' /etc/hosts` ]]; then  
+    echo "127.0.1.1 salt01" | tee -a /etc/hosts
   fi
 
 # Update Everything
@@ -36,9 +31,9 @@ salt_master=${2}
   apt-get -qq update
   apt-get -q dist-upgrade -y
 
-# Install Salt Minion
+# Install Salt Master and Minion
   echo_blue "Installing SaltStack if not installed"
-  if [[ ! $(dpkg -l | egrep 'salt-minion') ]]; then
+  if [[ ! $(dpkg -l | egrep 'salt-master|salt-minion') ]]; then
     if [[ ! $(apt-key list | grep "SaltStack Packaging Team") ]]; then
       wget -O - https://repo.saltstack.com/apt/ubuntu/16.04/amd64/latest/SALTSTACK-GPG-KEY.pub | sudo apt-key add -
     fi
@@ -46,15 +41,20 @@ salt_master=${2}
       echo "deb http://repo.saltstack.com/apt/ubuntu/16.04/amd64/latest xenial main" | tee -a /etc/apt/sources.list.d/saltstack.list
     fi
     apt-get update
-    apt-get install salt-minion git -y
+    apt-get install salt-master salt-minion git -y
   fi
 
 # Autoremove after installations
   echo_blue "Autoremoving no-longer-needed software"
   apt autoremove -y
 
-# Configure Salt Minion to talk to master
+# Configure Salt Minion to talk to local master
   echo_blue "Configuring local salt minion to talk to salt master"
-  if [[ ! $(grep "${salt_master}" /etc/hosts) ]]; then 
-    sed -ri "s/^127.0.0.1\s+localhost$/127.0.0.1\tlocalhost\n${salt_master}\ salt/g" /etc/hosts
-  fi
+  sed -ri 's/^127.0.0.1\s+localhost$/127.0.0.1\tlocalhost\ salt/g' /etc/hosts
+  systemctl restart salt-minion
+  sleep 15
+  salt-key -A -y
+
+# Set up KVM
+  echo_blue "Setting up salt master hypervisor"
+  salt 'salt*' state.apply compunaut_hypervisor.ssh,compunaut_hypervisor.kvm,compunaut_hypervisor.network
--- 03_compunaut_salt_minion.sh
+++ 03_compunaut_salt_minion.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 RED='\033[0;31m'
 BLUE='\033[0;34m'
 NC='\033[0m'
@@ -14,16 +15,20 @@ echo_blue() {
   echo -e "${BLUE}\n${message}...${NC}"
 }
 
-### INITIAL SALT MASTER SETUP
+### Get positional vars
+minion_hostname=${1}
+salt_master=${2}
+
+### INITIAL SALT SETUP
 # Update sudoers so that sudo group members don't need a password
   echo_blue "Updating sudoers"
   sed -ri 's/^\%sudo\s+ALL=\(ALL:ALL\)+\sALL$/\%sudo\tALL=\(ALL:ALL\)\ NOPASSWD:ALL/g' /etc/sudoers
 
 # Set up hostname
   echo_blue "Setting hostname if not set"
-  hostnamectl set-hostname salt01
-  if [[ ! `grep -P '127.0.1.1\s+salt01' /etc/hosts` ]]; then  
-    echo "127.0.1.1 salt01" | tee -a /etc/hosts
+  hostnamectl set-hostname ${minion_hostname}
+  if [[ ! `grep -P "127.0.1.1\s+${minion_hostname}" /etc/hosts` ]]; then  
+    echo "127.0.1.1 ${minion_hostname}" | tee -a /etc/hosts
   fi
 
 # Update Everything
@@ -31,9 +36,9 @@ echo_blue() {
   apt-get -qq update
   apt-get -q dist-upgrade -y
 
-# Install Salt Master and Minion
+# Install Salt Minion
   echo_blue "Installing SaltStack if not installed"
-  if [[ ! $(dpkg -l | egrep 'salt-master|salt-minion') ]]; then
+  if [[ ! $(dpkg -l | egrep 'salt-minion') ]]; then
     if [[ ! $(apt-key list | grep "SaltStack Packaging Team") ]]; then
       wget -O - https://repo.saltstack.com/apt/ubuntu/16.04/amd64/latest/SALTSTACK-GPG-KEY.pub | sudo apt-key add -
     fi
@@ -41,17 +46,16 @@ echo_blue() {
       echo "deb http://repo.saltstack.com/apt/ubuntu/16.04/amd64/latest xenial main" | tee -a /etc/apt/sources.list.d/saltstack.list
     fi
     apt-get update
-    apt-get install salt-master salt-minion git -y
+    apt-get install salt-minion git -y
   fi
 
 # Autoremove after installations
   echo_blue "Autoremoving no-longer-needed software"
   apt autoremove -y
 
-# Configure Salt Minion to talk to local master
+# Configure Salt Minion to talk to master
   echo_blue "Configuring local salt minion to talk to salt master"
-  sed -ri 's/^127.0.0.1\s+localhost$/127.0.0.1\tlocalhost\ salt/g' /etc/hosts
-  salt-key -A -y
-
-# Set up KVM
-  salt 'salt*' state.apply compunaut_hypervisor.ssh,compunaut_hypervisor.kvm,compunaut_hypervisor.network
+  if [[ ! $(grep "${salt_master}" /etc/hosts) ]]; then 
+    sed -ri "s/^127.0.0.1\s+localhost$/127.0.0.1\tlocalhost\n${salt_master}\ salt/g" /etc/hosts
+  fi
+  systemctl restart salt-minion
