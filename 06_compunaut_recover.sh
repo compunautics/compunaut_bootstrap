ce0731ecbd12cf182450880efebc35be2f1c9a39
Jesse Pendergrass <compunautics@gmail.com>
2018-11-19 18:51:59 -0600

    Renaming a lot of scripts

--- 06_compunaut_recover.sh
+++ 06_compunaut_recover.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # ensure all vms running
-salt -C 'salt* or kvm*' state.apply compunaut_hypervisor.vms,compunaut_consul
+salt -C '*salt* or *kvm*' state.apply compunaut_hypervisor.vms,compunaut_consul
 
 # recover databases
 salt '*db*' state.apply compunaut_mysql.galera,compunaut_influxdb
 
 # highstate everything else
-salt 'compunaut*' state.highstate
+salt '*compunaut*' state.highstate
