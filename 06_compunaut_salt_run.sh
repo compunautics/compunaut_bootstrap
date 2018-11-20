f56513c306c88768d87556e5fea95fc12b393680
Jesse Pendergrass <compunautics@gmail.com>
2018-11-19 19:05:04 -0600

    More renaming and mods

--- 06_compunaut_salt_run.sh
+++ 06_compunaut_salt_run.sh
@@ -4,7 +4,7 @@
 salt -C '*salt* or *kvm*' state.apply compunaut_hypervisor.vms,compunaut_consul
 
 # recover databases
-salt '*db*' state.apply compunaut_mysql.galera,compunaut_influxdb
+salt '*db*' state.apply compunaut_mysql.galera
 
 # highstate everything else
 salt '*compunaut*' state.highstate
