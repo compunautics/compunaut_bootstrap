a367d4ba30150ee7a06490f79d5fafd822df44ba
Jesse Pendergrass <compunautics@gmail.com>
2018-11-21 17:40:07 -0600

    Updating the salt run script

--- 06_compunaut_salt_run.sh
+++ 06_compunaut_salt_run.sh
@@ -1,10 +1,36 @@
 #!/bin/bash
 
+# functions
+update_data() {
+  minion_wait
+  echo_blue "Updating mine"
+  salt '*' mine.update
+  sleep 10
+
+  minion_wait
+  echo_blue "Updating pillar"
+  salt '*' saltutil.refresh_pillar
+  sleep 10
+}
+
+echo_red() {
+  local message=${1}
+  echo -e "${RED}\n${message}${NC}"
+}
+
+echo_blue() {
+  local message=${1}
+  echo -e "${BLUE}\n${message}...${NC}"
+}
+
 # ensure all vms running
-salt -C '*salt* or *kvm*' state.apply compunaut_hypervisor.vms,compunaut_consul
+echo_red "Highstate the Hypervisors"
+salt -C '*salt* or *kvm*' state.highstate
 
 # recover databases
+echo_red "Rebootstrap the MySQL Galera Cluster"
 salt '*db*' state.apply compunaut_mysql.galera
 
 # highstate everything else
+echo_red "Highstate the VMs"
 salt '*compunaut*' state.highstate
