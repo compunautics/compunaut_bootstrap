7d53342a74ba66e427057fe110c35bb1783ede3b
Jesse Pendergrass <compunautics@gmail.com>
2018-11-26 21:46:52 -0600

    Modifying the general salt run script

--- 06_compunaut_salt_run.sh
+++ 06_compunaut_salt_run.sh
@@ -4,25 +4,48 @@
 cd "${0%/*}"
 source ./compunaut_functions
 
-# ensure all vms running
+# Highstate the Hypervisors
 update_data
 echo_red "Highstate the Hypervisors"
 salt -C '*salt* or *kvm*' state.highstate
 
-# recover databases
+# Salt all VMs
 minion_wait
-echo_red "Bootstrap the MySQL Galera Cluster"
-salt '*db*' state.apply compunaut_mysql.galera
+echo_red "Salt all VMs"
+salt -C '*salt* or *kvm*' state.apply compunaut_hypervisor.salt_vms
 
-# highstate everything else
+sleep 30
+salt-key -A -y
+
+# Update all VMs
+minion_wait
+echo_red "Update targeted VM"
+salt -C "not *salt* and not *kvm*" cmd.run 'apt-get update && apt-get dist-upgrade -y'
+sleep 90
+
+# Configure Mine on all Nodes
+update_data
+echo_red "Configure mine on all Nodes"
+salt "*" state.apply compunaut_salt.minion
+sleep 60
+salt "*" saltutil.sync_all
+sleep 60
+
+# Highstate the VMs
+update_data
 echo_red "Highstate the VMs"
 salt -C 'not *salt* and not *kvm*' state.highstate
 
+# Bootstrap the MySQL Galera Cluster
 update_data
-sleep 60
+echo_red "Bootstrap the MySQL Galera Cluster"
+salt '*db*' state.apply compunaut_mysql.galera
 
+# Highstate the VMs again
+update_data
 echo_red "Highstate the VMs again"
 salt -C 'not *salt* and not *kvm*' state.highstate
 
+# Recover LDAP
 echo_red "Recover LDAP"
 salt '*ldap*' state.apply compunaut_openldap,compunaut_openldap.memberof,compunaut_openldap.repl
