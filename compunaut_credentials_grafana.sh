36611f84d5e84575c26366c0e5df5038349a9bfc
Jesse Pendergrass <compunautics@gmail.com>
2018-12-01 12:29:04 -0600

    Updating credentials to get from global_vars

--- compunaut_credentials_grafana.sh
+++ compunaut_credentials_grafana.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-salt '*monitor*' pillar.get grafana:server:admin
+salt '*monitor*' pillar.get compunaut:global_vars:grafana_admin_user
+salt '*monitor*' pillar.get compunaut:global_vars:grafana_unencrypted_admin_password
--- compunaut_credentials_rundeck.sh
+++ compunaut_credentials_rundeck.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-salt '*rundeck*' pillar.get rundeck:username
-salt '*rundeck*' pillar.get rundeck:password
+salt '*rundeck*' pillar.get compunaut:global_vars:rundeck_admin_user
+salt '*rundeck*' pillar.get compunaut:global_vars:rundeck_admin_unencrypted_password
