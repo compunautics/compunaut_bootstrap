61eb318f14d6f71a4c6cdce4ad051a88a93856eb
Jesse Pendergrass <compunautics@gmail.com>
2018-12-01 15:11:20 -0600

    reordering app installations

--- 05_compunaut_init_salt.sh
+++ 05_compunaut_init_salt.sh
@@ -92,15 +92,15 @@ source ./compunaut_functions
   echo_red "INSTALL GRAFANA"
   salt '*monitor*' state.apply compunaut_grafana -b1 --state_output=mixed --async
 
-# Install Gitlab
-  minion_wait
-  echo_red "INSTALL GITLAB"
-  salt '*gitlab*' state.apply compunaut_gitlab --state_output=mixed --async
-
 # Install Rundeck
   minion_wait
   echo_red "INSTALL RUNDECK"
   salt '*rundeck*' state.apply compunaut_rundeck -b1 --state_output=mixed --async
+
+# Install Gitlab
+  minion_wait
+  echo_red "INSTALL GITLAB"
+  salt '*gitlab*' state.apply compunaut_gitlab --state_output=mixed
 
 # Running highstate
   update_data
