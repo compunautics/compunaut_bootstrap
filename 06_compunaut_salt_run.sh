103c18636bd63aad34873853b38169d3545c1a48
Jesse Pendergrass <compunautics@gmail.com>
2018-12-02 00:41:38 -0600

    Updating format

--- 06_compunaut_salt_run.sh
+++ 06_compunaut_salt_run.sh
@@ -6,53 +6,60 @@ source ./compunaut_functions
 
 # Highstate the Hypervisors
 update_data
+
 echo_red "Highstate the Hypervisors"
 salt -C '*salt* or *kvm*' state.highstate --state_output=mixed
-sleep 20
 
 # Salt all VMs
 minion_wait
 echo_red "Salt all VMs"
 salt -C '*salt* or *kvm*' state.apply compunaut_hypervisor.salt_vms --state_output=mixed
 sleep 20
+
 salt-key -A -y
 
 # Update all VMs
 minion_wait
 echo_red "Update VMs"
-salt -C "not *salt* and not *kvm*" cmd.run 'apt-get update && apt-get dist-upgrade -y'
-sleep 60
+salt "*" cmd.run 'apt-get update && apt-get dist-upgrade -y'
 
 # Configure Mine on all Nodes
 update_data
+
 echo_red "Configure mine on all Nodes"
 salt "*" state.apply compunaut_salt.minion --state_output=mixed
 sleep 30
+
 salt "*" saltutil.sync_all
 sleep 30
 
 # Highstate the VMs
 update_data
+
 echo_red "Highstate the VMs"
 salt -C 'not *salt* and not *kvm*' state.highstate --state_output=mixed
 
 # Highstate the VMs again
 update_data
+
 echo_red "Highstate the VMs again"
 salt -C 'not *salt* and not *kvm*' state.highstate --state_output=mixed
 
 # Recover LDAP
 update_data
+
 echo_red "Recover LDAP"
 salt '*ldap*' state.apply compunaut_openldap,compunaut_openldap.memberof,compunaut_openldap.repl --state_output=mixed
 
 # Bootstrap the MySQL Galera Cluster
 update_data
+
 echo_red "Bootstrap the MySQL Galera Cluster"
 salt '*db*' state.apply compunaut_mysql.galera --state_output=mixed
 
 # Highstate the VMs one last time
 update_data
 sleep 30
+
 echo_red "Highstate the VMs one last time"
 salt -C 'not *salt* and not *kvm*' state.highstate --state_output=mixed
