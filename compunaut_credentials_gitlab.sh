f3279e0e9c2ec36948765f4fb82e9a83eaf514b5
Jesse Pendergrass <compunautics@gmail.com>
2018-12-02 00:43:44 -0600

    Correct credentials

--- compunaut_credentials_gitlab.sh
+++ compunaut_credentials_gitlab.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-salt '*gitlab*' pillar.get compunaut:global_vars:rundeck_admin_user
-salt '*gitlab*' pillar.get compunaut:global_vars:rundeck_admin_unencrypted_password
+salt '*gitlab*' pillar.get compunaut:global_vars:gitlab_admin_user
+salt '*gitlab*' pillar.get compunaut:global_vars:gitlab_admin_unencrypted_password
