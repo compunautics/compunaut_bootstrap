7643b36d450c938e419421833b3c798385874edf
Jesse Pendergrass <compunautics@gmail.com>
2018-12-03 16:35:23 -0600

    Updating script to try and shave off time

--- 05_compunaut_init_salt.sh
+++ 05_compunaut_init_salt.sh
@@ -9,18 +9,18 @@ source ./compunaut_functions
   update_data
 
   minion_wait
-  echo_blue "Highstate the kvm nodes"
+  echo_blue "Highstating the kvm nodes"
   salt -C '*salt* or *kvm*' state.highstate --state_output=mixed
 
 # Log into vms and configure salt
   minion_wait
-  echo_blue "Log into vms and configure hostname and salt"
+  echo_blue "Logging into vms and configuring hostname and salt"
   salt -C '*salt* or *kvm*' state.apply compunaut_hypervisor.salt_vms --state_output=mixed
 
 ### MINION SETUP
 # Accept all salt keys
   echo_red "SET UP COMPUNAUT MINIONS"
-  echo_blue "Accept salt keys from vms"
+  echo_blue "Accepting salt keys from vms"
   sleep 30
 
   salt-key -A -y
@@ -30,7 +30,7 @@ source ./compunaut_functions
   minion_wait
   echo_blue "Updating all vms"
   salt -C 'not *salt* and not *kvm*' cmd.run 'apt-get update && apt-get install salt-minion -y' --async
-  sleep 120
+  sleep 60
 
 # Configure mine on master and minions
   minion_wait
@@ -51,12 +51,8 @@ source ./compunaut_functions
   salt '*salt*' state.apply compunaut_openvpn.certificates --state_output=mixed
 
   minion_wait
-  echo_blue "Installing openvpn on vpn servers"
-  salt '*vpn*' state.apply compunaut_openvpn,compunaut_keepalived,compunaut_default --state_output=mixed
-
-  minion_wait
-  echo_blue "Installing openvpn on remaining vms"
-  salt -C 'not *salt* and not *kvm*' state.apply compunaut_openvpn,compunaut_default --state_output=mixed
+  echo_blue "Installing openvpn"
+  salt '*' state.apply compunaut_openvpn,compunaut_default --state_output=mixed
 
 # Install databases
   echo_red "INSTALL DATABASES"
@@ -64,7 +60,10 @@ source ./compunaut_functions
 
   minion_wait
   echo_blue "Installing MySQL, InfluxDB, and Influx Relay"
-  salt '*db*' state.apply compunaut_mysql,compunaut_influxdb --state_output=mixed
+  salt '*db*' state.apply compunaut_mysql,compunaut_influxdb --state_output=mixed --async
+
+  echo_blue "Installing LDAP"
+  salt '*ldap*' state.highstate --state_output=mixed --async
 
   update_data
 
@@ -72,21 +71,12 @@ source ./compunaut_functions
   echo_blue "Setting up Galera"
   salt '*db*' state.apply compunaut_mysql.galera --state_output=mixed
 
-# Install openldap
-  echo_red "INSTALL LDAP"
-  update_data
-
-  minion_wait
-  echo_blue "Apply states"
-  salt '*ldap*' state.apply compunaut_openldap,compunaut_openldap.memberof,compunaut_openldap.repl --state_output=mixed
-  salt '*ldap*' state.apply compunaut_openldap --state_output=mixed
-
 # Install consul
   echo_red "INSTALL CONSUL AND DNSMASQ"
   update_data
 
   minion_wait
-  echo_blue "Apply states"
+  echo_blue "Applying states"
   salt -C 'not *salt* and not *kvm*' state.apply compunaut_consul,compunaut_dnsmasq --state_output=mixed
 
 # Install Grafana
@@ -94,19 +84,19 @@ source ./compunaut_functions
   update_data
 
   minion_wait
-  echo_blue "Apply states"
+  echo_blue "Applying states"
   salt '*monitor*' state.apply compunaut_grafana --async
 
 # Install Rundeck
   echo_red "INSTALL RUNDECK"
 
-  echo_blue "Apply states"
+  echo_blue "Applying states"
   salt '*rundeck*' state.apply compunaut_rundeck --async
 
 # Install Gitlab
   echo_red "INSTALL GITLAB"
 
-  echo_blue "Apply states"
+  echo_blue "Applying states"
   salt '*gitlab*' state.apply compunaut_gitlab --async
   minion_wait
 
@@ -115,10 +105,10 @@ source ./compunaut_functions
   update_data
 
   minion_wait
-  echo_blue "Install telegraf"
+  echo_blue "Installing telegraf"
   salt '*' state.apply compunaut_telegraf --state_output=mixed
 
-  echo_blue "Run highstate"
+  echo_blue "Running highstate"
   salt -C 'not *salt* and not *kvm*' state.highstate --state_output=mixed
 
 # Final kvm node setup
