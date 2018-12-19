04170782a616f9b6e0daeda9cd4355129058461b
Jesse Pendergrass <compunautics@gmail.com>
2018-12-18 22:09:03 -0600

    Moving this minion_wait up here

--- 05_compunaut_init_salt.sh
+++ 05_compunaut_init_salt.sh
@@ -90,13 +90,13 @@ source ./compunaut_functions
 # Install Netboot
   echo_red "INSTALL NETBOOT"
 
+  minion_wait
   echo_blue "Applying states"
   salt -C 'I@compunaut_guacamole:* or I@compunaut_vnc:* or I@compunaut_piserver:*' state.apply compunaut_guacamole,compunaut_guacamole.mysql,compunaut_vnc,compunaut_piserver --async
 
 # Install Gitlab
   echo_red "INSTALL GITLAB"
 
-  minion_wait
   echo_blue "Applying states"
   salt -C 'I@gitlab:*' state.apply compunaut_gitlab --async
 
