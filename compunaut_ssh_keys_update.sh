a10a43911fba7aa5952fc53ea784ac509902e9db
Jesse Pendergrass <compunautics@gmail.com>
2018-12-22 19:19:47 -0600

    Updating this to deploy rundeck keys

--- compunaut_ssh_keys_update.sh
+++ compunaut_ssh_keys_update.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+### FUNCTIONS
+cd "${0%/*}"
+source ./compunaut_functions
 
-salt 'salt*' state.apply compunaut_hypervisor.ssh
-salt 'compunaut*' state.apply compunaut_default.ssh
+### CREATE AND DEPLOY KEYS
+  echo_red "CREATE AND DEPLOY KEYS"
+  minion_wait
+
+  echo_blue "Creating default users"
+  salt '*' state.apply compunaut_default.users --state_output=mixed
+  echo_blue "Creating keys"
+  salt '*salt*' state.apply compunaut_hypervisor.ssh,compunaut_rundeck.keys --state_output=mixed
+  echo_blue "Deploying private rundeck key"
+  salt -C 'I@rundeck:*' state.apply compunaut_rundeck.private --state_output=mixed
+  echo_blue "Deploying public keys"
+  salt '*' state.apply compunaut_default.ssh,compunaut_rundeck.public --state_output=mixed
