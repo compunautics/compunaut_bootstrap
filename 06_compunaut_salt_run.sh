a608bfc4be0775f5bc97f13c9d11a0ff3de9e66b
Jesse Pendergrass <compunautics@gmail.com>
2018-12-29 14:49:28 -0600

    Updating with update data and minion wait

--- 06_compunaut_salt_run.sh
+++ 06_compunaut_salt_run.sh
@@ -4,7 +4,11 @@
 cd "${0%/*}"
 source ./compunaut_functions
 
+  update_data
   ./compunaut_init_salt_create_vms.sh
+  minion_wait
   ./compunaut_init_salt_highstate.sh
+  minion_wait
   ./compunaut_init_salt_install_dbs.sh
+  minion_wait
   ./compunaut_init_salt_highstate.sh
