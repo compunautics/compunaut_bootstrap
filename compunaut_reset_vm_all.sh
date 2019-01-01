715ce052fe49f060467a1b6c369aff22377227ff
Jesse Pendergrass <compunautics@gmail.com>
2018-12-31 20:03:29 -0600

    Going to specify vms and printers moving forward

--- compunaut_reset_vm_all.sh
+++ compunaut_reset_vm_all.sh
@@ -4,7 +4,8 @@
 salt -C 'salt* or kvm*' state.sls compunaut_hypervisor.reset --state_output=mixed
 
 # delete all keys for all minions (except salt and kvm nodes)
-salt-key -d '*compunaut*' -y
+salt-key -d '*vm*' -y
+salt-key -d '*prtr*' -y
 
 # delete all openvpn certs
 rm -fv /srv/repos/compunaut_openvpn/salt/keys/*
