354212baf57c63649b8dacc5861330777ab950a8
Jesse Pendergrass <compunautics@gmail.com>
2019-01-10 12:13:02 -0600

    Updating to delete prtr entries correctly

--- compunaut_reset_vm_all.sh
+++ compunaut_reset_vm_all.sh
@@ -5,7 +5,7 @@ salt -C 'salt* or kvm*' state.sls compunaut_hypervisor.reset --state_output=mixe
 
 # delete all keys for all minions (except salt and kvm nodes)
 salt-key -d '*-vm*' -y
-salt-key -d '*-prtr*' -y
+salt-key -d '*prtr*' -y
 
 # delete all openvpn certs
 rm -fv /srv/repos/compunaut_openvpn/salt/keys/*
