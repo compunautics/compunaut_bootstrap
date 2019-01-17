a6a0c173ef904dd90d73f26b0f7c0abdba7dc2a4
Jesse Pendergrass <compunautics@gmail.com>
2019-01-16 20:36:55 -0600

    Change this script to platform recover

--- compunaut_platform_recover.sh
+++ compunaut_platform_recover.sh
@@ -4,10 +4,6 @@
 cd "${0%/*}"
 source ./compunaut_functions
 
-  ./compunaut_init_salt_create_vms.sh
-  minion_wait
-  ./compunaut_init_salt_highstate.sh
-  minion_wait
   ./compunaut_init_salt_install_dbs.sh
   minion_wait
   ./compunaut_init_salt_highstate.sh
