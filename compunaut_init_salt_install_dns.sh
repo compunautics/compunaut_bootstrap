eb735ab998695b60dbc90b92dd256a717066c1dc
Jesse Pendergrass <compunautics@gmail.com>
2019-01-26 23:16:57 -0600

    Switching back

--- compunaut_init_salt_install_dns.sh
+++ compunaut_init_salt_install_dns.sh
@@ -4,10 +4,10 @@ cd "${0%/*}"
 source ./compunaut_functions
 
 # Install dnsmasq
-  echo_red "INSTALL DNSMASQ"
+  echo_red "INSTALL CONSUL AND DNSMASQ"
 
   echo_blue "Applying states"
-  salt -C 'not I@compunaut_hypervisor:*' state.apply compunaut_dnsmasq -b8 --batch-wait 25 --state_output=mixed
+  salt -C 'not I@compunaut_hypervisor:*' state.apply compunaut_consul,compunaut_dnsmasq -b8 --batch-wait 25 --state_output=mixed
 
   echo_blue "Restarting dnsmasq"
   salt '*' cmd.run 'systemctl restart dnsmasq'
--- compunaut_init_salt_install_openvpn.sh
+++ compunaut_init_salt_install_openvpn.sh
@@ -4,13 +4,13 @@ cd "${0%/*}"
 source ./compunaut_functions
 
 # Install openvpn
-  echo_red "DEPLOY CONSUL AND OPENVPN"
+  echo_red "DEPLOY OPENVPN"
 
   echo_blue "Generating OpenVPN certs for minions"
   salt '*salt*' state.apply compunaut_openvpn.ca,compunaut_openvpn.certificates --state_output=mixed
 
   minion_wait
-  echo_blue "Deploying Consul and OpenVPN"
+  echo_blue "Deploying OpenVPN"
   salt -C 'I@openvpn:*' state.apply compunaut_consul,compunaut_openvpn,compunaut_default -b8 --batch-wait 25 --state_output=mixed
 
   echo_blue "Restarting OpenVPN"
