37fac8f055a429345d70587f5e87b9b9c2544760
Jesse Pendergrass <compunautics@gmail.com>
2019-01-27 17:54:09 -0600

    Decrease batch

--- compunaut_init_salt_install_dns.sh
+++ compunaut_init_salt_install_dns.sh
@@ -7,7 +7,7 @@ source ./compunaut_functions
   echo_red "INSTALL CONSUL AND DNSMASQ"
 
   echo_blue "Applying states"
-  salt -C 'not I@compunaut_hypervisor:*' state.apply compunaut_consul,compunaut_dnsmasq -b8 --batch-wait 25 --state_output=mixed
+  salt -C 'not I@compunaut_hypervisor:*' state.apply compunaut_consul,compunaut_dnsmasq -b6 --batch-wait 25 --state_output=mixed
 
   echo_blue "Restarting dnsmasq"
   salt '*' cmd.run 'systemctl restart dnsmasq'
