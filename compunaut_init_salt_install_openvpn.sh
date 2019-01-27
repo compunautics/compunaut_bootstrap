72d9759c5e664f154b34614bbd8b9d16aa81933e
Jesse Pendergrass <compunautics@gmail.com>
2019-01-26 21:40:15 -0600

    Reordering things

--- compunaut_init_salt_install_openvpn.sh
+++ compunaut_init_salt_install_openvpn.sh
@@ -4,14 +4,14 @@ cd "${0%/*}"
 source ./compunaut_functions
 
 # Install openvpn
-  echo_red "DEPLOY OPENVPN"
+  echo_red "DEPLOY CONSUL AND OPENVPN"
 
   echo_blue "Generating OpenVPN certs for minions"
   salt '*salt*' state.apply compunaut_openvpn.ca,compunaut_openvpn.certificates --state_output=mixed
 
   minion_wait
-  echo_blue "Deploying OpenVPN"
-  salt -C 'I@openvpn:*' state.apply compunaut_openvpn,compunaut_default -b8 --batch-wait 25 --state_output=mixed
+  echo_blue "Deploying Consul and OpenVPN"
+  salt -C 'I@openvpn:*' state.apply compunaut_consul,compunaut_openvpn,compunaut_default -b8 --batch-wait 25 --state_output=mixed
 
   echo_blue "Restarting OpenVPN"
   salt -C 'I@openvpn:*' cmd.run 'systemctl restart openvpn'
