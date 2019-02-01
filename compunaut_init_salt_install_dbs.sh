3d3a5256d076dcab42b2c638623b81ad89db6dd1
Jesse Pendergrass <compunautics@gmail.com>
2019-01-31 22:13:54 -0600

    Updating

--- compunaut_init_salt_install_dbs.sh
+++ compunaut_init_salt_install_dbs.sh
@@ -25,7 +25,7 @@ minion_wait() {
   salt -C 'I@mysql:server:*' state.apply compunaut_mysql,compunaut_influxdb --async
 
   echo_blue "Installing LDAP"
-  salt -C 'I@openldap:slapd_services:*' state.apply compunaut_openvpn.deploy,compunaut_openldap -b1 --state_output=mixed
+  salt -C 'I@openldap:slapd_services:*' state.apply compunaut_openvpn.deploy,compunaut_openldap --state_output=mixed
   echo_green "Waiting 45 seconds"
   sleep 45
 
