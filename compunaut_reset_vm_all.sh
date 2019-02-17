4f08f1ca648998f718f0c933c60b4035e0a02467
Jesse Pendergrass <compunautics@gmail.com>
2019-02-17 14:46:47 -0600

    Updating this to handle the new paths

--- compunaut_reset_vm_all.sh
+++ compunaut_reset_vm_all.sh
@@ -4,14 +4,14 @@ cd "${0%/*}"
 source ./compunaut_functions
 
 # reset all vms on all hypervisors
-salt -C 'salt* or kvm*' state.sls compunaut_hypervisor.reset --state_output=mixed
+salt -C 'salt* or kvm*' state.sls compunaut_kvm.reset --state_output=mixed
 
 # delete all keys for all minions (except salt and kvm nodes)
 salt-key -d '*-vm*' -y
 salt-key -d '*prtr*' -y
 
 # delete all openvpn certs
-rm -fv /srv/repos/compunaut_openvpn/salt/keys/*
+rm -fv /srv/compunaut_pki/keys/*
 
 # reset consul
 minion_wait
